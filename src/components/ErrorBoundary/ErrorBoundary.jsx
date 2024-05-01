82190b0b701570c46e355fabd116804dc2431be2
Meet Kalani <meetkalani2002@gmail.com>
2024-05-01 09:47:15 +0530

    remove react-error-boundary dependency with custom implementation

---
+++ src/components/ErrorBoundary/ErrorBoundary.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ErrorPage from "../../pages/ErrorPage/ErrorPage";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error("Error caught by boundary:", error, errorInfo);
+  }
+
+  render() {
+    const { hasError } = this.state;
+    const { children } = this.props;
+
+    if (hasError) {
+      return <ErrorPage />;
+    }
+
+    return children;
+  }
+}
+
+ErrorBoundary.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default ErrorBoundary;
